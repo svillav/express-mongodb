171822b9e454ee55409c2cd8493ef8a9e3ef0603
Santiago Villa <santiagovillavega@gmail.com>
2020-08-11 18:59:57 -0300

    Test

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
-
 const CommentSchema = new Schema({
-    content: { type: String, required: true}
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true},
 },{timestamps:true});
 
-
-export default mongoose.model('Comment', CommentSchema);
+module.exports = mongoose.model('Comment', CommentSchema);
--- models/user.model.js
+++ models/user.model.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true},
+    password: {type: String, required: true}
 });
-
 
 module.exports = mongoose.model('User', UserSchema);
