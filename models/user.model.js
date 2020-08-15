9c3cbc8e3039d1cb56df5f216c91b6435db32170
Santiago Villa <santiagovillavega@gmail.com>
2020-08-15 00:34:28 -0300

    User Model file modified

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const UserModel = new Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true}
+    email: {type: String, required: true, unique: true, lowercase: true},
+    password: {type: String, required: true, select: false},
+    signupDate: {type: Date, default: Date.now()},
+    lastLogin: Date
 });
+
+UserModel.pre('save', next => {
+    let user = this;
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        })
+    })
+})
 
 module.exports = mongoose.model('UserModel', UserModel);
